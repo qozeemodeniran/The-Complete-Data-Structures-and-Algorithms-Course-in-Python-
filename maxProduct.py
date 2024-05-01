a37ba902be2883c8c2a8a3a55bb346d73a115bf0
Qozeem Odeniran <muhdqozeem@gmail.com>
2024-04-30 22:44:31 -0400

    simple update

--- maxProduct.py
+++ maxProduct.py
@@ -1,9 +1,7 @@
 """
 Max Product of Two Integers
 Find the maximum product of two integers in an array where all elements are positive.
-
 Example
-
 arr = [1, 7, 3, 4, 9, 5]
 max_product(arr) # Output: 63 (9*7)
 """
