57ee75cd5a0ce579c269d5feebb20f9025f78124
Qozeem Odeniran <muhdqozeem@gmail.com>
2024-05-04 21:55:20 -0400

    solved the 'finding the pairs problem'

--- pairs.py
+++ pairs.py
@@ -17,3 +17,15 @@ Note:
 
 def pair_sum(myList, sum):
     # TODO
+    used = []
+    result = []
+    for i in range(len(myList)):
+        for j in range(i+1, len(myList)):
+            if myList[i] + myList[j] == sum:
+                result.append(f"{myList[i]} + {myList[j]}")
+                #used.append(f"{myList[i]},{myList[j]}")
+    return result #used
+
+print(pair_sum([2, 4, 3, 5, 6, -2, 4, 7, 8, 9],7))
+
+
